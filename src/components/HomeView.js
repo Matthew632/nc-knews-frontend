1bfb457170f1a11364a590c28cedb01c143d35e0
Matthew632 <matthew.hebb@hotmail.co.uk>
2019-05-09 22:54:07 +0100

    adds previews to top articles

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -4,10 +4,15 @@ import Topics from "./Topics";
 import AddArticle from "./AddArticle";
 import { Link, navigate } from "@reach/router";
 import { Container, Row, Col } from "react-bootstrap";
+import "../style.css";
 
 class HomeView extends Component {
   state = {
-    topArticles: null
+    topArticles: null,
+    previewText: null,
+    previewArr: null,
+    change: false,
+    previewChange: false
   };
 
   render() {
@@ -15,10 +20,9 @@ class HomeView extends Component {
       <div>
         <h1>NC News</h1>
         <Topics wording="Search by Topic" />
-        <h2>Top Articles</h2>
-        <Container>
-          <Row>
-            <Col>Title</Col>
+        <Container className="topArticles">
+          <Row className="addArticleHeader">
+            <Col>Top Articles</Col>
             <Col>Comments</Col>
           </Row>
           {this.state.topArticles &&
@@ -32,7 +36,8 @@ class HomeView extends Component {
                   >
                     {art.title}
                   </Link>
-                </Col>{" "}
+                  {this.state.previewText && <p className = "preview">{this.state.previewText[`id${art.article_id}`]}</p>}
+                  </Col>{" "}
                 <Col>{art.comment_count}</Col>
               </Row>
             ))}
@@ -41,11 +46,23 @@ class HomeView extends Component {
       </div>
     );
   }
+
+
+    shorten = obj => {
+        const body = obj.article.body;
+        const shortened = body.length < 50 ? body : `${body.substring(0,body.substring(0,49).lastIndexOf(' '))}...`;
+        return shortened
+  }
+
+  getPreviews = arr => {
+    Promise.all([fetchData(`/articles/${this.state.topArticles[0].article_id}`),fetchData(`/articles/${this.state.topArticles[1].article_id}`),fetchData(`/articles/${this.state.topArticles[2].article_id}`)]).then(([zero,one,two]) => {this.setState(prevState => ({ ...prevState, previewArr: [zero,one,two], previewChange: true}))
+  })
+}
+
   componentDidMount = () => {
     fetchData("/articles?sort_by=comment_count&order=desc&limit=3")
       .then(data => {
-        this.setState({ topArticles: data.articles });
-      })
+        this.setState({ topArticles: data.articles, change: true })})
       .catch(error => {
         navigate("/error", {
           replace: true,
@@ -60,10 +77,20 @@ class HomeView extends Component {
 
   componentDidUpdate(prevState) {
     if (this.state.change) {
+      this.getPreviews(this.state.topArticles)
       this.setState(prevState => ({
         ...prevState,
         change: false
-      }));
+      }))
+    }
+    if (this.state.previewChange) {
+      const snippets = {};
+      this.state.previewArr.forEach(obj => snippets[`id${obj.article.article_id}`] = this.shorten(obj));
+      this.setState(prevState => ({
+        ...prevState,
+        previewText: snippets,
+        previewChange: false
+      }))
     }
   }
 
